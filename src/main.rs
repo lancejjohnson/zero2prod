6ae1d57f06c99514a9717348fa103dfe13464b76
Lance Johnson <lancejjohnson@gmail.com>
2022-07-01 16:26:29 -0400

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use tokio;
 
 // async fn greet(req: HttpRequest) -> impl Responder {
@@ -12,11 +12,8 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
